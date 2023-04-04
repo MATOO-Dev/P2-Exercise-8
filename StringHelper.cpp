2a7776b0061ffccdc54d2cd29ac0f21ef83f07c8
MATOO-Dev <fagoldob@gmail.com>
2023-04-04 19:36:13 +0200

    deleted old files

--- StringHelper.cpp
+++
@@ -1,9 +0,0 @@
-#include "StringHelper.h"
-
-
-
-
-bool StringHelper::endsWith(const string& target, const string& ending) {
-    auto pos = target.rfind(ending);
-    return pos == target.length() - ending.length();
-}

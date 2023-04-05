7600e29c2f94e0ff729f264d53de9d962e6b3f27
MATOO-Dev <fagoldob@gmail.com>
2023-04-05 13:05:31 +0200

    Backup push of linux port before vacation

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include "include/glad.h"
 #include <GLFW/glfw3.h>
+#include "include/ViewPortGL.h"
+
+void test() {
+    ViewPortGL vp = ViewPortGL("OpenGL Plain Test", 1000, 1000);
+    bool isRunning = true;
+    while (isRunning) {
+        isRunning = !vp.windowShouldClose();
+
+        vp.prepareTriangle(0, 999, 999, 999, 500, 0, 200, 0, 0, 0, 0, 200, 0, 200, 0);
+        vp.sendTriangles();
+
+        vp.swapBuffers();
+    }
+}
 
 int main()
 {
     std::cout << "Hello World!" << std::endl;
+    test();
     
     return 0;
 }
